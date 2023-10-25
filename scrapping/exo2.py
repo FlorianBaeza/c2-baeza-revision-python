86de3fab40300d2d92ac4cf60747c4ec256fa4a5
FlorianBaeza <baeza.florian@gmail.com>
2023-10-25 12:24:50 +0200

    scrapping exo2

---
+++ scrapping/exo2.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def countries_capitals_list():
+    '''
+    Affiche la liste des pays présente à l'URL spécifiée
+    '''
+
+    url = 'https://www.scrapethissite.com/pages/simple/'
+    page = requests.get(url)
+    soup = bs(page.content, 'html.parser')
+
+    for country in soup.find_all('div', {'class': 'country'}):
+        country_formatted: str = country.text.strip()
+        
+        country_name = country_formatted.split("\n")[0]
+        country_capital = country_formatted.split("\n")[3].split("Capital: ")[1]
+
+        print(country_name + ", " + country_capital)
+
+        
+countries_capitals_list()
