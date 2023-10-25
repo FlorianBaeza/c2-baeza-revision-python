f564c912c468ea47ed817b009d7120bf7a5a326d
FlorianBaeza <baeza.florian@gmail.com>
2023-10-25 09:52:45 +0200

    exo3 Calculatrice

---
+++ e3_calculatrice.py
@@ -0,0 +1,26 @@
+def calcul_simple(elements: list):
+    '''
+    Entrée : elements, la liste contenant les deux nombres ainsi que le signe d'opération
+    Sortie : /
+
+    Effectue un calcul simple via un opérateur entre deux nombres
+    '''
+
+    nombre_1 = (int)(elements[0])
+    nombre_2 = (int)(elements[2])
+    operateur = (elements[1])
+
+    if (operateur == "+"):
+        print(nombre_1 + nombre_2)
+    elif (operateur == "-"):
+        print(nombre_1 - nombre_2)
+    elif (operateur == "*"):
+        print(nombre_1 * nombre_2)
+    elif (operateur == "/"):
+        print(nombre_1 / nombre_2)
+
+# ===========================================================
+
+calcul = str(input("Veuillez entrer une opération simple (addition, soustraction, multiplication, division) : "))
+calcul_list: list = calcul.split(" ")
+calcul_simple(calcul_list)
